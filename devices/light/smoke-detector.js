ff7a88b8b366e79d9b6cdf86a1991059de56df31
Jacco Huysmans <jacco.huijsmans@p-direkt.minbzk.nl>
2019-04-30 16:35:10 +0200

    wip

--- devices/light/smoke-detector.js
+++ devices/light/smoke-detector.js
@@ -4,19 +4,25 @@ module.exports = function (RED) {
   'use strict';
   let mustache = require('mustache');
 
-  function XiaomiCubeNode(config) {
+  function XiaomiSmokeDetectorNode(config) {
     RED.nodes.createNode(this, config);
     this.gateway = RED.nodes.getNode(config.gateway);
     this.sid = config.sid;
     this.output = config.output;
+    this.openmsg = config.openmsg;
+    this.closemsg = config.closemsg;
 
     let node = this;
     let persistent = {
+      'status': null,
       'previous_status': null,
       'voltage': null,
-      'voltage_level': null
+      'voltage_level': null,
+      'alarm': null,
+      'density':null
     };
 
+    //initial status
     node.status({fill: 'grey', shape: 'ring', text: 'battery'});
 
     if (this.gateway) {
@@ -24,7 +30,7 @@ module.exports = function (RED) {
       node.on('input', function (msg) {
         let payload = msg.payload;
 
-        if (payload.sid === node.sid && payload.model.indexOf('cube') >= 0) {
+        if (payload.sid === node.sid && payload.model.indexOf('smoke') >= 0) {
           let result = null;
           let data = payload.data;
 
@@ -35,28 +41,49 @@ module.exports = function (RED) {
             persistent.voltage = info.voltage;
             persistent.voltage_level = info.voltage_level;
           }
-
+          if (data.alarm) {
+            persistent.alarm = parseInt(data.alarm);
+            switch (persistent.alarm){
+              case 0:
+                persistent.status = "standby";
+                break;
+              case 2:
+                persistent.status = "alarm";
+                break;
+              case 8:
+                persistent.status = "Low battery";
+                break;
+              default:
+                persistent.status = "unknown";
+                break;
+            }
+          }
+          if (data.density) {
+            persistent.density = parseInt(data.density);
+          }
           if (node.output === '0') {
             //raw data
             result = payload;
           } else if (node.output === '1') {
-            //values
+            //only values
             result = Object.assign({
-              status: null,
-              duration: null,
-              lux: null
+              alarm: null,
+              density: null
             }, persistent);
 
             if (data.status) {
               result.status = data.status;
             }
-            if (data.no_cube) {
-              result.status = 'no_cube';
-              result.duration = data.no_cube;
-            }
 
             result.time = new Date().getTime();
             result.device = self.gateway.getDeviceName(self.sid);
+          } else if (node.output === '2') {
+            //template
+            if (data.status && data.status === 'open') {
+              result = mustache.render(node.openmsg, data);
+            } else {
+              result = mustache.render(node.closemsg, data);
+            }
           }
 
           msg.payload = result;
@@ -73,5 +100,5 @@ module.exports = function (RED) {
     }
   }
 
-  RED.nodes.registerType('magic-cube-controller', XiaomiCubeNode);
+  RED.nodes.registerType('xiaomi-smoke-detector', XiaomiSmokeDetectorNode);
 };
