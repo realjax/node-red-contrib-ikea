2362475a919e55a76da73f45cea992adf3df1984
Jacco Huysmans <jacco.huijsmans@p-direkt.minbzk.nl>
2019-06-26 16:34:37 +0200

    wip

--- devices/gateway/gateway.js
+++ devices/gateway/gateway.js
@@ -1,16 +1,8 @@
-'use strict';
+"use strict";
 
-const crypto = require('crypto');
-
-const TradfriClient = require('node-tradfri-client');
+const TC = require("node-tradfri-client");
 
 
-/**
- * @param <Buffer> message
- * @param <Object> rinfo
- *
- * @returns Object
- */
 function createMessage(message, rinfo) {
   let payload = JSON.parse(message.toString());
   if (payload.data) {
@@ -25,142 +17,136 @@ function createMessage(message, rinfo) {
   };
 }
 
-/**
- * @param {string} key
- * @param {Object} msg
- * @returns {String}
- */
-function getGatewayToken(key, msg) {
-  let result = null;
 
-  //get gateway key
-  if (key) {
-    let token = msg.payload.token;
-    if (token) {
-      let cipher = 123;
-      let encoded_string = cipher.update(token, 'utf8', 'hex');
 
-      encoded_string += cipher.final('hex');
-      result = encoded_string.substring(0, 32);
-    }
-  }
 
-  return result;
-}
-
-/**
- * @param {RED} RED
- */
 module.exports = function (RED) {
-  let udpInputPortsInUse = {};
 
-  /**
-   * @param {Object} config
-   * @constructor
-   */
   function IkeaGatewayNode(config) {
-    RED.nodes.createNode(this, config);
+    var node = this;
+    RED.nodes.createNode(node, config);
+    var tradfri = null;
+    var _clientConnected = false;
+    var _lights = {};
+    var _listeners = {};
 
-    this.gateway = RED.nodes.getNode(config.gateway);
-    this.healthcheck = parseInt(config.healthcheck) || 60;
+    node.gateway = RED.nodes.getNode(config.gateway);
+    node.healthcheck = parseInt(config.healthcheck) || 60;
 
-    let currentToken = null;
-    let timer;
-
-    if (this.gateway && this.gateway.address && this.gateway.port) {
+    if (node.gateway && node.gateway.address && node.gateway.psk && node.gateway.identity) {
       //initial status
-      this.status({fill: 'grey', shape: 'ring', text: 'not connected'});
+      node.status({fill: "grey", shape: "ring", text: "not connected"});
 
-      //initialize connection
-      let socket;
-      let reuse = false;
-      if (!udpInputPortsInUse.hasOwnProperty(this.port)) {
-       // socket = dgram.createSocket({type: 'udp4'});  // default to udp4
-       // socket.bind(this.gateway.port);
-       // udpInputPortsInUse[this.port] = socket;
+      tradfri = new TC.TradfriClient(this.gateway.address,{watchConnection:{maximumReconnects: 5,maximumConnectionAttempts: 5}});
+
+
+      tradfri.connect(this.gateway.identity, this.gateway.psk).then(
+        (connected) => {
+          node.status({fill: "green", shape: "ring", text: "connected"});
+          debuglog("connected is "+connected);
+          _clientConnected = connected;
+           tradfri.observeGateway().then(()=>debuglog("observing gateway")).catch(err => console.log("foutje hier"));
+           tradfri.observeNotifications(()=>debuglog("observing notifications")).catch(()=>debuglog("observing notifications failed"));
+           tradfri.observeGroupsAndScenes(()=>debuglog("observing groups and scenes")).catch(()=>debuglog("observing groups and scenes failed"));
+           tradfri.observeDevices(()=>{}).catch(()=>debuglog("observingDevices failed"));
+          },
+        (connected) => {
+          node.status({fill: "red", shape: "ring", text: "connection error"});
+          debuglog("could not connect to gateway.");
+        }).catch(err => {console.log("foutje")});
+
+
+    }else{
+      node.status({fill: "grey", shape: "ring", text: "not configured"});
+    }
+
+
+
+    // tradfri events
+    tradfri
+      .on("gateway updated", (gw) => {
+        node.status({text: "connected (v. "+gw.version+")"});
+      })
+      .on("rebooting", (reason) => {
+        this.status({fill: "red", shape: "ring", text: "rebooting"});
+        debuglog("rebooting gateway, reason: ${reason}");
+      })
+      .on("device updated", _deviceUpdatedCallback)
+      .on("ping failed", (count) => console.log(`${count} pings failed`))
+      .on("ping succeeded", () => console.log("ping succeeded"))
+      .on("connection lost", () => console.log("connection lost"))
+      .on("connection failed", (att, max) => console.log(`connection failed: attempt ${att} of ${max}`))
+      .on("connection alive", () => console.log("connection alive"))
+      .on("gateway offline", () => console.log("gateway offline"))
+      .on("reconnecting", (att, max) => console.log(`reconnect attempt ${att} of ${max}`))
+      .on("give up", () => console.log("giving up..."))
+      .on("device updated", (acc) => console.log(`device with ID ${acc.instanceId} updated...`));
+
+
+
+    node.on("close", function(removed, done) {
+      if (removed) {
+        console.log("node removed")
       } else {
-        this.log('UDP socket is aleady used, try reusing existing', this.port);
-        socket = udpInputPortsInUse[this.port];  // re-use existing
-        reuse = true;
+        // This node is being restarted
+        console.log("node restarted")
+        if (_clientConnected) {
+          tradfri.reset();
+        }
+        destroyClient();
+      }
+      done();
+    });
+
+    node.on("input", (msg) => {
+      if (!msg.hasOwnProperty("payload")) { return; } // do nothing unless we have a payload
+      if (_clientConnected) {
+        let cmd = msg.payload;
+        switch (cmd.toUpperCase()) {
+          case "REBOOT":
+              tradfri.rebootGateway().then(()=>debuglog("reboot has started"),()=>debuglog("reboot failed to start")).catch(err => console.log(err));
+            break;
+          case "DEVICES":
+            for (let k in _lights) {
+              console.log({type: _lights[k].type,name: _lights[k].name, id: k});
+            }
+            break;
+        }
       }
 
-      socket.on('listening', () => {
-        if (false === reuse) {
-          socket.addMembership(this.gateway.address);
-        }
+      return [null];
+    })
 
-        //debug
-        const address = socket.address();
-        this.log(`UDP socket listening on ${address.address}:${address.port}`);
-
-        //initial status
-        this.status({fill: 'green', shape: 'ring', text: 'connected'});
-      });
-
-      socket.on('message', (message, rinfo) => {
-        let msg = createMessage(message, rinfo)
-
-        if (msg.payload.cmd == 'heartbeat' && msg.payload.model == 'gateway') {
-          //get token
-          currentToken = getGatewayToken(this.gateway.key, msg);
-
-          //healthcheck
-          this.status({fill: 'green', shape: 'ring', text: 'connected'});
-
-          clearTimeout(timer);
-          timer = setTimeout(() => {
-            this.status({fill: 'red', shape: 'ring', text: 'healthcheck failed'});
-          }, this.healthcheck * 1000);
-
-          return;
-        }
-
-        //send message
-        if (msg.payload.cmd == 'report' || msg.payload.cmd == 'heartbeat') {
-          this.send([msg]);
-        }
-      });
-
-      //listen for node close message and free socket
-      this.on("close", () => {
-        if (udpInputPortsInUse.hasOwnProperty(this.port)) {
-          delete udpInputPortsInUse[this.port];
-        }
-        try {
-          socket.close();
-          this.log('UDP socket closed');
-        } catch (err) {
-          //this.error(err);
-        }
-      });
-
-      //listen for incomming messages
-      this.on('input', (msg) => {
-        if (this.gateway.key && currentToken) {
-          let cmd = msg.payload;
-          cmd.data.key = currentToken;
-          cmd.data = JSON.stringify(cmd.data);
-
-          const message = Buffer.from(JSON.stringify(cmd));
-          socket.send(message, 0, message.length, this.gateway.port, this.gateway.address, () => {
-            this.log(`Sending message '${message}'`);
-          });
-        } else {
-          this.status({fill: 'red', shape: 'ring', text: 'key is not set'});
-        }
-
-        return [null];
-      })
-    } else {
-      //initial status
-      this.status({fill: 'grey', shape: 'ring', text: 'not configured'});
+    function debuglog(message){
+      RED.log.debug(node.type +" : "+message);
     }
+
+    function errorlog(message){
+      RED.log.error(node.type +" : "+message);
+    }
+
+    function destroyClient() {
+      console.log("we here")
+      if (_clientConnected) {
+        client.destroy();
+      }
+    }
+    function _deviceUpdatedCallback(accessory){
+      if (accessory.type === TC.AccessoryTypes.lightbulb) {
+        _lights[accessory.instanceId] = accessory;
+      }
+      //if (_listeners[accessory.instanceId]) {
+      //  for (let nodeId in _listeners[accessory.instanceId]) {
+      //    _listeners[accessory.instanceId][nodeId](accessory);
+      //  }
+
+    }
+
   }
 
- //gets sockets
-  var discovered,discover,connect,testTradfri,error;
 
-  RED.httpAdmin.get('/ikea-gateway', RED.auth.needsPermission('ikea-gateway.read'), (req, res) => {
+
+  RED.httpAdmin.get("/ikea-gateway", RED.auth.needsPermission("ikea-gateway.read"), (req, res) => {
 
     var discover = async function(res) {
       try {
@@ -174,21 +160,39 @@ module.exports = function (RED) {
     discover(res);
   });
 
+  RED.httpAdmin.get("/ikea-gateway-devices", RED.auth.needsPermission("ikea-gateway-devices.read"), (req, res) => {
 
-  RED.httpAdmin.get('/ikea-gateway-connect', RED.auth.needsPermission('ikea-gateway.connect'), (req, res) => {
+   var getDevices = async function(res) {
+      try {
+        let ret = [];
+        for (let k in _lights) {
+          ret.push({name: _lights[k].name, id: k});
+        }
+        return  res.json(JSON.stringify(ret));
+      } catch (e){
+        console.log(e);
+        return res.json([{"error": "no devices found"}]);
+      }
+    };
+    getDevices(res,req);
+  });
+
+
+  RED.httpAdmin.get("/ikea-gateway-connect", RED.auth.needsPermission("ikea-gateway.connect"), (req, res) => {
     var connect = async function(res,req) {
-      testTradfri = new TradfriClient.TradfriClient(req.query.address);
+      var error;
+      var testTradfri = new TC.TradfriClient(req.query.address);
       try {
         var keys = await testTradfri.authenticate(req.query.code);
         return res.json(keys);
       } catch (e) {
-        if (e instanceof TradfriClient.TradfriError) {
+        if (e instanceof TC.TradfriError) {
           switch (e.code) {
-            case TradfriClient.TradfriErrorCodes.ConnectionTimedOut: {
-              error = "Couldn't connect, check IP/Host";
+            case TC.TradfriErrorCodes.ConnectionTimedOut: {
+              error = "Could not connect, check IP/Host";
               break;
             }
-            case TradfriClient.TradfriErrorCodes.AuthenticationFailed: {
+            case TC.TradfriErrorCodes.AuthenticationFailed: {
               error = "The security code is wrong";
               break;
             }
@@ -203,5 +207,5 @@ module.exports = function (RED) {
 });
 
   //register new type
-  RED.nodes.registerType('ikea-gateway', IkeaGatewayNode);
+  RED.nodes.registerType("ikea-gateway", IkeaGatewayNode);
 };
